e321a1ebf83ee48884d3ef0c4a49fc0ee02eb83e
CrazyDiamond777 <152586419+CrazyDiamond777@users.noreply.github.com>
2023-12-20 18:08:52 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,20 @@ class User < ApplicationRecord
 
   validates :nick_name, presence: true
   validates :birth_date, presence: { message: "date can't be blank" }
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[A-Za-z0-9]+\z/.freeze
+  validates :birth_date, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[A-Za-z0-9]+\z/
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'} do
-  validates :last_name
-  validates :first_name
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :last_name
+    validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'} do
-  validates :last_name_kana
-  validates :first_name_kana
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
   end
-
+  has_many :items
+  has_many :purchase_records
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :user do
-    nick_name {'ニックネーム'}
-    first_name {'名'}
-    last_name {'姓'}
-    first_name_kana {'メイ'}
-    last_name_kana {'セイ'}
-    birth_date {'2000-01-01'}
-    email {'test@example'}
-    password {'pass123'}
-    
+    nick_name { 'ニックネーム' }
+    first_name { '名' }
+    last_name { '姓' }
+    first_name_kana { 'メイ' }
+    last_name_kana { 'セイ' }
+    birth_date { '2000-01-01' }
+    email { 'test@example' }
+    password { 'pass123' }
   end
 end
